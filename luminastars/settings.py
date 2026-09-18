ed2c0d24c3b01696a8bf069096d710e0e8f5c0b6
chriskay0x <christiankalu020@gmail.com>
2026-09-18 23:09:00 +0100

    Edited the Settings file

--- luminastars/settings.py
+++ luminastars/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-luminastars-change-in
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.vercel.app']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
